38cfc9cc37103b2d26cbf55be053aeeb1d73dd49
Bordignon Schuvantek Nunes <robu@gft.com>
2022-02-23 01:17:50 -0300

    initial commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
