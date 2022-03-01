63aba34b6e6ff3bbb38fa2dc77b8e2cc0650c626
Bordignon Schuvantek Nunes <robu@gft.com>
2022-03-01 14:52:17 -0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import { Room } from "./pages/Room";
-//test//
 function App() {
   return (
     <BrowserRouter>
